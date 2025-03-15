12eeac50174e28bf420a2f3e7a5cce043dc1b716
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-03-15 19:48:10 +0800

    chore: Add icon module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
   integrations: [
+    icon(),
     UnoCSS({
       injectReset: true,
     }),
