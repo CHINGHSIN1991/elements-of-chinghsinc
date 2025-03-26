e97c1b57bf9fd8300d855758edd9febc383ac141
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-03-26 23:39:26 +0800

    feat: Add cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
 import icon from 'astro-icon'
 import UnoCSS from 'unocss/astro'
 import react from '@astrojs/react'
 
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [
     icon(),
     UnoCSS({
