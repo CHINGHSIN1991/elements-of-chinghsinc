dfb2c81f20d3e54d26ed230d6191b800d71ffe52
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-06 23:38:53 +0800

    chore: Add actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,20 @@ import icon from 'astro-icon'
 import UnoCSS from 'unocss/astro'
 import react from '@astrojs/react'
 
+const LOCAL_URL = 'http://localhost:4321'
+const LIVE_URL = 'https://CHINGHSIN1991.github.io'
+const SCRIPT = process.env.npm_lifecycle_script || ''
+const isBuild = SCRIPT.includes('astro build')
+
+let url = LOCAL_URL
+if (isBuild) {
+  url = LIVE_URL
+}
+
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
+  site: url,
   integrations: [
     icon(),
     UnoCSS({
