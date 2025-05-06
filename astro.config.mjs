708ea06a8fe637da633e3f33de8ed84e24a61499
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-06 23:49:39 +0800

    chore: Modify typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import UnoCSS from 'unocss/astro'
 import react from '@astrojs/react'
 
 const LOCAL_URL = 'http://localhost:4321'
-const LIVE_URL = 'https://CHINGHSIN1991.github.io'
+const LIVE_URL = 'https://chinghsin1991.github.io'
 const SCRIPT = process.env.npm_lifecycle_script || ''
 const isBuild = SCRIPT.includes('astro build')
 
