2ed786c595d89adf4b9552b696d02521ea087cca
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-14 23:31:37 +0800

    chore: Add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import UnoCSS from 'unocss/astro'
 import react from '@astrojs/react'
 
 const LOCAL_URL = 'http://localhost:4321'
-const LIVE_URL = 'https://chinghsin1991.github.io'
+const LIVE_URL = 'https://chinghsin1991.github.io/element-of-chinghsinc'
 const SCRIPT = process.env.npm_lifecycle_script || ''
 const isBuild = SCRIPT.includes('astro build')
 
