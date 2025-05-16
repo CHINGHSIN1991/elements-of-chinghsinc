c3cc5942e390f903456e8b77dd70ed1bbd28edbf
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-05-16 23:29:02 +0800

    chore: Modify site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,15 @@ import react from '@astrojs/react'
 
 const LOCAL_URL = 'http://localhost:4321'
 const LIVE_URL = 'https://chinghsin1991.github.io'
+const REPO_NAME = 'elements-of-chinghsinc'
 const SCRIPT = process.env.npm_lifecycle_script || ''
 const isBuild = SCRIPT.includes('astro build')
 
 let url = LOCAL_URL
 let base = '/'
 if (isBuild) {
-  url = LIVE_URL
-  base = '/elements-of-chinghsinc/'
+  url = `${LIVE_URL}/${REPO_NAME}`
+  base = `/${REPO_NAME}/`
 }
 
 export default defineConfig({
