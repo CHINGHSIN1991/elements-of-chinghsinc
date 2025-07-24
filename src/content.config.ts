72975340949560d1b6a82360a5f678a39d800069
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-07-24 23:25:26 +0800

    refactor: Modify content schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const posts = defineCollection({
   type: 'content',
   schema: z.object({
-    id: z.string(),
+    id: z.string().optional(),
     title: z.string(),
     description: z.string(),
     date: z.string(),
@@ -34,4 +34,49 @@ const projects = defineCollection({
   })
 });
 
-export const collections = { posts, projects };
+
+const educationSchema = z.array(z.object({
+  school: z.string(),
+  degree: z.string(),
+  location: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
+  description: z.string()
+}));
+
+const workExperienceSchema = z.array(z.object({
+  company: z.string(),
+  position: z.string(),
+  location: z.string(),
+  startDate: z.string(),
+  endDate: z.string(),
+  description: z.array(z.string())
+}));
+
+const skillsSchema = z.object({
+  architecture: z.record(z.any()),
+  software: z.record(z.any())
+});
+
+const contactInfoSchema = z.object({
+  email: z.string().email(),
+  GitHub: z.string().url(),
+  LinkedIn: z.string().url(),
+  about: z.string()
+});
+
+const static_content = defineCollection({
+  type: 'data',
+  schema: z.discriminatedUnion('_type', [
+    z.object({ _type: z.literal('education'), data: educationSchema }),
+    z.object({ _type: z.literal('workExperience'), data: workExperienceSchema }),
+    z.object({ _type: z.literal('skills'), data: skillsSchema }),
+    z.object({ _type: z.literal('contactInfo'), data: contactInfoSchema })
+  ])
+});
+
+export const collections = { 
+  posts,
+  projects,
+  static: static_content
+};
